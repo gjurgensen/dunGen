2ba0944508986f6662026e9ec1ca427f23e82bdb
Grant Jurgensen <gajurgensen@gmail.com>
2020-05-12 12:39:40 -0500

    Working hallway gen

--- src/Dungeon.hs
+++ src/Dungeon.hs
@@ -1,9 +1,13 @@
 module Dungeon where
 
 import System.Random
-import Control.Type.Operator
+import Control.Monad
+--import Control.Applicative
+--import Control.Type.Operator
+import Data.Bool
 import Data.Matrix
 import Data.Maybe
+--import Data.Function
 
 import Misc
 
@@ -18,41 +22,67 @@ dunTile a b c = go
 
 type Dungeon = Matrix DunTile
 
-
 cmdLineRender :: Dungeon -> IO ()
 cmdLineRender = putStrLn . unlines
               . fmap (unwords . (fmap $ dunTile "." "#" "x")) . toLists
 
-dunGen :: Int -> Int -> Int -> IO Dungeon
-dunGen gas sizeX sizeY = go gas $ matrix sizeY sizeX $ const Wall
+
+data Dir = U | R | D | L
+
+dir a b c d = go
   where
-    sizeMin = 3
-    sizeMax = 10
+    go U = a
+    go R = b
+    go D = c
+    go L = d
 
-    go :: Int -> Dungeon -> IO Dungeon
-    go n dun
-      | n <= 0   = return dun
-      | otherwise = do
-          room <- genRoom
-          if or $ elementwise collision room dun
-            then go (n-1) dun
-            else go gas $ combine room dun
+dirToVec = dir (0,-1) (1,0) (0,1) (-1,0)
+
+rotClockwise = dir R D L U
+rotCounter   = dir L U R D
+
+type Worm = (Int, Coord, Dir)
+
+dunGen :: Int -> Int -> Int -> IO Dungeon
+dunGen gas sizeX sizeY = 
+  let center = ((sizeX -1) `div` 2, (sizeY -1) `div` 2) in
+  tunnel [(gas, center, U), (gas, center, R),
+          (gas, center, D), (gas, center, L)]
+       $ matrix sizeY sizeX $ const Wall
+  where
+    tunnel :: [Worm] -> Dungeon -> IO Dungeon
+    tunnel []    dun = return dun
+    tunnel worms dun = do
+      worms1 <- join <$> (sequence $ plan <$> worms)
+      let worms2 = act <$> worms1
+      tunnel worms2 $ foldr dig dun worms2
+
+    plan :: Worm -> IO [Worm]
+    plan (life, coord, dir) =
+      if life == 0 then
+        return mzero
+      else do
+        let worm = (life -1, coord, dir)
+        r <- randomRIO (0,29) :: IO Int
+        --return $ case r of
+        --  0 -> [worm, rotWormClockwise worm]
+        --  1 -> [worm, rotWormCounter   worm]
+        --  2 -> [rotWormClockwise worm]
+        --  3 -> [rotWormCounter   worm]
+        --  _ -> [worm]
+        return $ if r < 1 then [worm, rotWormClockwise worm] else
+                 if r < 2 then [worm, rotWormCounter   worm] else
+                 if r < 4 then [rotWormClockwise worm] else
+                 if r < 6 then [rotWormCounter   worm] else
+                 [worm]
       where
-        collision mx y = isJust mx && y == Room
-        combine = elementwise orElse
+        rotWormClockwise (l, c, dir) = (l, c, rotClockwise dir)
+        rotWormCounter   (l, c, dir) = (l, c, rotCounter   dir)
 
-    genRoom :: IO $ Matrix $ Maybe DunTile
-    genRoom = do
-      roomSizeX <- randomRIO (sizeMin, sizeMax)
-      roomSizeY <- randomRIO (sizeMin, sizeMax)
-      roomX <- randomRIO (0, sizeX - roomSizeX)
-      roomY <- randomRIO (0, sizeY - roomSizeY)
-      return $ matrix sizeY sizeX $ \p ->
-        let coord = fromMatCoord p in
-        if coord `inRect`
-          ((roomX, roomY), (roomX + roomSizeX -1, roomY + roomSizeY -1))
-        then Just Room else
-        if coord `inRect`
-          ((roomX -1, roomY -1), (roomX + roomSizeX, roomY + roomSizeY))
-        then Just Wall else
-        Nothing
+    act (life, coord, dir) = (life, move dir coord, dir)
+         
+    dig (_, (x,y), _) dun = adjSetElem dun x y Hall
+
+    move :: Dir -> Coord -> Coord
+    move dir coord = restrictRect (0,0) (sizeX -1,sizeY -1)
+                   $ liftPair2 (+) coord $ dirToVec dir
--- src/Misc.hs
+++ src/Misc.hs
@@ -2,11 +2,25 @@ module Misc where
 
 import Data.Matrix
 
+type Coord = (Int,Int)
+
+liftPair  f (x0,y0) = (f x0, f y0)
+liftPair2 f (x0,y0) (x1,y1) = (f x0 x1, f y0 y1)
+liftPair3 f (x0,y0) (x1,y1) (x2,y2) = (f x0 x1 x2, f y0 y1 y2)
+
 -- *inclusive* range check
 x `inRange` (min, max) = x >= min && x <= max
 
 (x,y) `inRect` ((minX,minY),(maxX,maxY)) = x `inRange` (minX, maxX)
                                         && y `inRange` (minY, maxY)
+
+restrict min max x
+  | x < min   = min
+  | x > max   = max
+  | otherwise = x
+
+restrictRect (minX,minY) (maxX,maxY) (x,y) = (restrict minX maxX x,
+                                              restrict minY maxY y)
 
 mx `orElse` y = maybe y id mx
 
@@ -16,3 +30,4 @@ fromMatCoord (i, j) = (j - 1, i - 1)
 toMatCoord   (x, y) = (y + 1, x + 1)
 
 adjGetElem m x y = uncurry getElem (toMatCoord (x,y)) m
+adjSetElem m x y elem = setElem elem (toMatCoord (x,y)) m
